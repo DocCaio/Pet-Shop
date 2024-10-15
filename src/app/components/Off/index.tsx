51ecb3f29f3e50ec9d4dac302bd9369e29556b2b
Caio 😊 <108128119+DocCaio@users.noreply.github.com>
2024-10-15 16:36:36 -0300

    up

---
+++ src/app/components/Off/index.tsx
@@ -0,0 +1,15 @@
+
+import styles from './off.module.css';
+
+
+
+export default function Shows() {
+  return (
+    <section className={styles.container}>
+      <span>
+         New Styles Added: Save Up to 60%
+         See more
+      </span>
+    </section>
+  );
+}
