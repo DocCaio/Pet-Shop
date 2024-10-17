85eceaada2d403b0d44a98ce7eb1531e3b72f4f4
Caio 😊 <108128119+DocCaio@users.noreply.github.com>
2024-10-17 10:01:38 -0300

    up

---
+++ src/app/components/Hero/index.tsx
@@ -0,0 +1,15 @@
+
+import styles from './Hero.module.css';
+
+
+
+export default function Off() {
+  return (
+    <section className={styles.container}>
+      
+      <div>
+      Redefining Motion: The Future of Footwear is Here
+      </div>
+    </section>
+  );
+}
