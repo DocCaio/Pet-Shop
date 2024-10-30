63e2f782c3cf063d9511f650b17731286fadb0eb
Caio 😊 <108128119+DocCaio@users.noreply.github.com>
2024-10-30 08:19:16 -0300

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,20 +7,24 @@ import Slides from "./components/Slides";
 import Slides2 from "./components/Slides2";
 import Banner from './components/Banner';
 import List from './components/List';
+import List2 from './components/List2';
 
 export default function Home() {
   return (
-    <div className={styles.page}>      
-     <Off/>
-     <NavBar/>
-     <main>
-     <Hero/>
-     <Performace/>
-     <Slides/>
-     <Slides2/>
-     <Banner/>
-     <List/>
-     </main>
+    <div className={styles.page}>
+      <Off />
+      <NavBar />
+      <main>
+        <Hero />
+        <Performace />
+        <Slides />
+        <Slides2 />
+        <Banner />
+        <List />
+      </main>
+        <List2 />
+
+
     </div>
   );
 }
