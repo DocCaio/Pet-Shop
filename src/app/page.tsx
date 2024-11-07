d5dd52ef1d3a02f798b17b838b14fa03cf148cc1
Caio 😊 <108128119+DocCaio@users.noreply.github.com>
2024-11-07 08:04:20 -0300

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import NavBar from './components/NavBar';
 import Hero from './components/Hero';
 import Services from './components/Services';
-
+import Plains from './components/Plains';
 
 import styles from "./page.module.css";
 
@@ -10,14 +10,15 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-     
-     <NavBar/>
+
+      <NavBar />
       <main>
-        <Hero/>
-        <Services/>
-      
+        <Hero />
+        <Services />
+        <Plains />
+
       </main>
-      
+
 
 
     </div>
