76ee590db1514e3edb982eea7033c0f3eed9a9ae
Caio 😊 <108128119+DocCaio@users.noreply.github.com>
2024-11-15 07:49:11 -0300

    up

--- src/app/components/Services/index.tsx
+++ src/app/components/Services/index.tsx
@@ -1,28 +1,29 @@
-import styles from './Services.module.css'
+import Link from 'next/link';
+import styles from './Services.module.css';
 
 export default function Services() {
-    return (
-        <>
-         <section className={styles.servicos}>
-      <h2 className={styles.servicosTitulo}>Nossos serviços</h2>
-      <ul className={styles.servicosLista}>
-        <li className={styles.banhoETosa}>
-          <a className={styles.link} href="#">Banho e Tosa</a>
-        </li>
-        <li className={styles.hospedagem}>
-          <a className={styles.link}href="#">Hospedagem</a>
-        </li>
-        <li className={styles.veterinario}>
-          <a className={styles.link} href="#">Veterinário</a>
-        </li>
-        <li className={styles.adestramento}>
-          <a className={styles.link} href="#">Adestramento</a>
-        </li>
-        <li className={styles.petsiter}>
-          <a className={styles.link} href="#">Petsiter</a>
-        </li>
-      </ul>
-    </section>
-        </>
-    )
+  return (
+    <>
+      <section className={styles.servicos}>
+        <h2 className={styles.servicosTitulo}>Nossos serviços</h2>
+        <ul className={styles.servicosLista}>
+          <li className={styles.banhoETosa}>
+            <Link className={styles.link} href={'#'} >Banho e Tosa</Link>
+          </li>
+          <li className={styles.hospedagem}>
+            <Link className={styles.link} href={'#'} >Hospedagem</Link>
+          </li>
+          <li className={styles.veterinario}>
+            <Link className={styles.link} href={'#'} >Veterinário</Link>
+          </li>
+          <li className={styles.adestramento}>
+            <Link className={styles.link} href={'#'} >Adestramento</Link>
+          </li>
+          <li className={styles.petsiter}>
+            <Link className={styles.link} href={'#'} >Petsiter</Link>
+          </li>
+        </ul>
+      </section>
+    </>
+  )
 }
