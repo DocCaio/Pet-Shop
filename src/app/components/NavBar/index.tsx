31e8df2e0ab49d8c8911ff776d804e55e32393d6
Caio 😊 <108128119+DocCaio@users.noreply.github.com>
2024-11-25 09:59:18 -0300

    up

--- src/app/components/NavBar/index.tsx
+++ src/app/components/NavBar/index.tsx
@@ -2,6 +2,7 @@ import styles from './NavBar.module.css';
 import Image from 'next/image';
 import Logo from '../../assets/logo.png';
 import Login from '../../assets/icones/login.png';
+import Link from 'next/link';
 
 export default function NavBar() {
     return (
@@ -21,21 +22,22 @@ export default function NavBar() {
       </button>
     </div>
     <nav className={styles. navegacao} id={styles.container}>
-      <ul className="menu-lista">
-        <li className="menu-item">
-          <a className={styles.menuLink} href="#">Raças</a>
+      <ul className={styles.menuLista}>
+        <li className= {styles.menuItem}>
+          <Link className={styles.menuLink} href="#">Raça</Link>        
         </li>
-        <li className="menu-item">
-          <a className={styles.menuLink}  href="#">Planos Pet</a>
+        <li className= {styles.menuItem}>
+        <Link className={styles.menuLink} href="#">Planos Pet</Link>           
         </li>
-        <li className="menu-item">
-          <a className={styles.menuLink}  href="#">Serviços</a>
+        <li className= {styles.menuItem}>
+        <Link className={styles.menuLink} href="#">Serviços</Link>      
         </li>
-        <li className="menu-item">
-          <a className={styles.menuLink}  href="#">Sobre nós</a>
+        <li className= {styles.menuItem}>
+        <Link className={styles.menuLink} href="#">Sobre nós</Link>      
         </li>
-        <li className="menu-item">
-          <a className={styles.menuLink}  href="#">Clube de descontos</a>
+        <li className= {styles.menuItem}>
+        <Link className={styles.menuLink} href="#">Clube de descontos</Link> 
+       
         </li>
       </ul>
     </nav>
